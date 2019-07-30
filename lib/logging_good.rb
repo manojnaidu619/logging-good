baab744660b67da95fce002ac9ee378172a35ec6
Manoj Naidu <manojnaidu619@gmail.com>
2019-07-30 13:20:47 +0530

    User input validation done

--- lib/logging_good.rb
+++ lib/logging_good.rb
@@ -1,18 +1,55 @@
 require "logging_good/version"
-require 'colorize'
 require 'terminal-table'
 
 class Now
 
-  def self.logging_good(params)
-     rows = params
-     #logger = ActiveSupport::Logger.new(STDOUT)
-     #logger.info (@params.inspect).colorize(:green)
-     table = Terminal::Table.new :rows => rows
-     puts table
-     #puts (@params.inspect).colorize(:green)
+  #def self.logging_good(params=nil, req=nil, response=nil)
+  def self.logging_good(*args)
 
-     #puts @params["controller"]
+    raise_error if args.empty? or args.count > 3
+
+    args.each do |i|
+        #raise_error(1) if i.class != ActionController::Parameters or i.class != ActionDispatch::Request
+        if [ActionController::Parameters, ActionDispatch::Request].each{|j| j==i.class }
+          parameters_table(i) if i.key?("controller")
+          request_table(i) if i.key?("REQUEST_URI")
+        else
+          raise_error(1)
+        end
+    end
+
   end
+end
 
+private
+
+def raise_error(line = 0)
+  raise ArgumentError, 'Wrong number of Arguments (args = [params, request, response])' if line.eql?(0)
+  raise TypeError, "Argument passed is not of type Hash" if line.eql?(1)
+end
+
+def generate_table(row_data, title)
+  Terminal::Table.new :title => title, :rows => row_data
+end
+
+def parameters_table(params)
+   rows = params
+   table = generate_table(rows, "PARAMETERS")
+   puts table
+end
+
+def request_table(request)
+    req = request.env
+    new_req = Hash.new
+    new_req = {REQUEST_METHOD: req["REQUEST_METHOD"],
+               REQUEST_URI: req["REQUEST_URI"],
+               HTTP_REFERER: req["HTTP_REFERER"],
+               HTTP_VERSION: req["HTTP_VERSION"],
+               HTTP_ACCEPT: req["HTTP_ACCEPT"],
+               HTTP_USER_AGENT: req["HTTP_USER_AGENT"],
+               SERVER_NAME: req["SERVER_NAME"],
+               SERVER_SOFTWARE: req["SERVER_SOFTWARE"]
+              }
+    table = generate_table(new_req, "REQUEST")
+    puts table
 end
--- logging_good.gemspec
+++ logging_good.gemspec
@@ -25,7 +25,10 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 2.0"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
-  spec.add_development_dependency "colorize", "~> 0.8.1"
   spec.add_development_dependency "unicode-display_width", '~> 1.6'
   spec.add_development_dependency "terminal-table", '~> 1.8', '>= 1.8.0'
+
+  spec.add_runtime_dependency "terminal-table", '~> 1.8'
+  spec.add_runtime_dependency "unicode-display_width", '~> 1.6'
+
 end
