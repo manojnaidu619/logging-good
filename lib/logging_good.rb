d10b645a57b53513a8a5ddd300de4569404ad5de
Manoj Naidu <manojnaidu619@gmail.com>
2019-07-30 09:10:27 +0530

    Added terminal-table

--- lib/logging_good.rb
+++ lib/logging_good.rb
@@ -1,11 +1,18 @@
 require "logging_good/version"
-require "active_support/logger"
 require 'colorize'
+require 'terminal-table'
 
 class Now
+
   def self.logging_good(params)
-     @params = params
-     logger = ActiveSupport::Logger.new(STDOUT)
-     logger.info (@params.inspect).colorize(:green)
+     rows = params
+     #logger = ActiveSupport::Logger.new(STDOUT)
+     #logger.info (@params.inspect).colorize(:green)
+     table = Terminal::Table.new :rows => rows
+     puts table
+     #puts (@params.inspect).colorize(:green)
+
+     #puts @params["controller"]
   end
+
 end
--- logging_good.gemspec
+++ logging_good.gemspec
@@ -25,6 +25,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 2.0"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
-  spec.add_development_dependency 'activesupport', '~> 5.0', '>= 5.0.0'
-  spec.add_development_dependency "colorize"
+  spec.add_development_dependency "colorize", "~> 0.8.1"
+  spec.add_development_dependency "unicode-display_width", '~> 1.6'
+  spec.add_development_dependency "terminal-table", '~> 1.8', '>= 1.8.0'
 end
